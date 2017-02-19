31574b3575a846644a1ad28ef640949a079ba07b
xianyang <xianyang@MBP-xy.local>
2017-02-19 18:12:31 +0800

    Add request view

---
+++ Wil_User/Wil_User/Views/RequestValetPopup.h
@@ -0,0 +1,18 @@
+//
+//  RequestValetPopup.h
+//  Wil_User
+//
+//  Created by xianyang on 19/02/2017.
+//  Copyright © 2017 xianyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RequestValetPopup : UIView
+
+//- (id)initWithFrame:(CGRect)frame;
+
+- (void)setDropOffPoint:(NSString *)place;
+- (void)setPaymentMethod:(NSString *)payment;
+
+@end
